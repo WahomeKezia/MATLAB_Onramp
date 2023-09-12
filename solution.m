75d572265ccf66a59bcaa1a7da05f2c26446c834
KeziaWahome <k.wahome@alustudent.com>
2023-09-12 11:28:01 +0200

    updates

--- solution.m
+++ solution.m
@@ -1,3 +1,6 @@
+% In the previous project, you calculated the speed (in km/s) of a star relative to Earth by using its spectrum. In this project, you will calculate all the stars' speeds at once. Then you'll create this plot of the star spectra.
+
+
 %This code loads the data and defines measurement parameters.
 load starData
 nObs = size(spectra,1)
@@ -19,3 +22,15 @@ ylabel("Intensity")
 %Find the wavelength of the hydrogen-alpha line by using idx to index into lambda. Store the result as lambdaHa (λHa) 
 [sHa , idx ] = min(s)
 lambdaHa = lambda(idx)
+
+% Add a point to the existing axes by plotting x = lambdaHa, y = sHa as a red square ("rs") with a marker size ("MarkerSize") of 8.
+hold on
+plot(lambdaHa,sHa,"rs","MarkerSize",8)
+hold off
+
+% Calculate the redshift factor (z) and the speed (in km/s) at which the star is moving away from Earth. Assign the redshift factor to a variable named z and the speed to a variable named speed.
+
+z = lambdaHa/656.28 - 1
+speed = z*299792.458
+
+% Having the script , you can easily change the the value of s to get the results of a different star 
